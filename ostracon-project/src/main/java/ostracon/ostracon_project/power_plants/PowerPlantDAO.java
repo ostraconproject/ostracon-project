9332dd2eea18ae4a17ab070c50d53b4e2011c7f2
Nick <nickmarlor@hotmail.com>
2018-01-29 14:51:11 +0000

    task2- created DAO searches to find all power plants by country and find all power plants by country and year. These are to be used with services.

--- ostracon-project/src/main/java/ostracon/ostracon_project/power_plants/PowerPlantDAO.java
+++ ostracon-project/src/main/java/ostracon/ostracon_project/power_plants/PowerPlantDAO.java
@@ -1,7 +1,23 @@
 package ostracon.ostracon_project.power_plants;
 
+import java.util.List;
+
 import com.googlecode.genericdao.dao.jpa.GenericDAO;
 
-public interface PowerPlantDAO extends GenericDAO<PowerPlant, Long> {
-
+public interface PowerPlantDAO extends GenericDAO<PowerPlant, Long> 
+{
+	/**
+	 * For finding all of the power plants within a country.
+	 * @param country
+	 * @return power plants
+	 */
+	List<PowerPlant> findPowerPlantsByCountry(String country);
+	
+	/**
+	 * For finding all the power plants within a country for a particular year.
+	 * @param country
+	 * @param year
+	 * @return power plants
+	 */
+	List<PowerPlant> findPowerPlantsByCountryAndYear(String country, Integer year);
 }
--- ostracon-project/src/main/java/ostracon/ostracon_project/power_plants/PowerPlantDAOImpl.java
+++ ostracon-project/src/main/java/ostracon/ostracon_project/power_plants/PowerPlantDAOImpl.java
@@ -1,10 +1,29 @@
 package ostracon.ostracon_project.power_plants;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
+
+import com.googlecode.genericdao.search.Search;
 
 import ostracon.ostracon_project.lib.HibernateJPABase;
 
 @Repository
 public class PowerPlantDAOImpl extends HibernateJPABase<PowerPlant, Long> implements PowerPlantDAO {
+
+	@Override
+	public List<PowerPlant> findPowerPlantsByCountry(String country) {
+		Search search = new Search(PowerPlant.class);
+		search.addFilterEqual("country", country);
+		return super.search(search);
+	}
+
+	@Override
+	public List<PowerPlant> findPowerPlantsByCountryAndYear(String country, Integer year) {
+		Search search = new Search(PowerPlant.class);
+		search.addFilterEqual("country", country);
+		search.addFilterEqual("year", year);
+		return super.search(search);
+	}
 
 }
